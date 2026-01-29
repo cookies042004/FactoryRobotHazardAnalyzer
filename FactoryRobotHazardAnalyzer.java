ac7ce27dd678bb7c46a0f563e27d9e776cd44f78
Akhil Puri <ap4723@srmist.edu.in>
2026-01-29 17:04:57 +0530

    [Akhil] Added: UC1: Displaying the purpose of the system

---
+++ FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,14 @@
+/**
+* Factory Robot Hazard Analyzer UC1 - Printing message using print statement
+*
+* This class display the static message indicating the purpose of the system.
+*
+* @Developer
+* @version 1.0
+* */
+
+public class FactoryRobotHazardAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
