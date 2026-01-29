b12a8d4bc62a7ee12b9b75ff316e10f751fbdb5a
Akhil Puri <ap4723@srmist.edu.in>
2026-01-29 19:49:43 +0530

    [Akhil] Added: UC2: Displaying the Factory Robot data

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 /**
-* Factory Robot Hazard Analyzer UC1 - Printing message using print statement
-*
-* This class display the static message indicating the purpose of the system.
-*
-* @Developer
-* @version 1.0
+ * Factory Robot Hazard Analyzer UC1 - Printing message using print statement
+ *
+ * This class will take the input from the user. All data required for hazard analysis
+ * and display that input data as output.
+ *
+ * @Developer
+ * @version 2.0
 * */
 
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
-        System.out.println("Factory Robot Hazard Analyzer");
+        Scanner sc = new Scanner(System.in);
+
+        // Taking Arm Precision value from user
+        System.out.println("Enter Arm precision (0.0 - 1.0): ");
+        double armPrecision = sc.nextDouble();
+
+        // Taking Worker Density value from user
+        System.out.println("Enter Worker Density (1 - 20): ");
+        int workerDensity = sc.nextInt();
+
+        sc.nextLine();
+
+        // Taking Machinery State value from user
+        System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
+        String machineState = sc.nextLine();
+
+        System.out.println("Arm Precision : " + armPrecision);
+        System.out.println("Worker Density : " + workerDensity);
+        System.out.println("Machinery State : "  + machineState);
     }
 }
